b35bef14c35ce015da347888b1b1a3dfa1066ed6
ABOBA23462 <116728011+ABOBA23462@users.noreply.github.com>
2023-02-08 10:04:28 +0600

    Update MainActivity.kt

--- app/src/main/java/com/example/kotlin2lvl_2less/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/kotlin2lvl_2less/ui/activity/MainActivity.kt
@@ -5,16 +5,27 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.kotlin2lvl_2less.R
+import com.example.kotlin2lvl_2less.utils.PreferenceHelper
 
 class MainActivity : AppCompatActivity() {
 
+    private val preferenceHelper = PreferenceHelper()
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         setupNavigation()
+        isShown()
+    }
+
+    private fun isShown() {
+        preferenceHelper.unit(this)
+        if (!preferenceHelper.onSaveOnBoardState) {
+            navController.navigate(R.id.noteAppFragment)
+        } else {
+            navController.navigate(R.id.onBoardFragment)
+        }
     }
 
     private fun setupNavigation() {
