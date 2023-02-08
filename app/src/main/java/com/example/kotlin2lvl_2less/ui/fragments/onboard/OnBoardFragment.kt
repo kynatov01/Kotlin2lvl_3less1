b78963407a48afc8abe311448d629fe252ac7e72
ABOBA23462 <116728011+ABOBA23462@users.noreply.github.com>
2023-02-08 10:08:40 +0600

    Update OnBoardFragment.kt

--- app/src/main/java/com/example/kotlin2lvl_2less/ui/fragments/onboard/OnBoardFragment.kt
+++ app/src/main/java/com/example/kotlin2lvl_2less/ui/fragments/onboard/OnBoardFragment.kt
@@ -1,5 +1,4 @@
 package com.example.kotlin2lvl_2less.ui.fragments.onboard
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,7 +8,8 @@ import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.widget.ViewPager2
 import com.example.kotlin2lvl_2less.databinding.FragmentOnBoardBinding
-import com.example.kotlin2lvl_2less.ui.adapter.OnBoardViewPagerAdapter
+import com.example.kotlin2lvl_2less.ui.adapters.OnBoardViewPagerAdapter
+import com.example.kotlin2lvl_2less.utils.PreferenceHelper
 
 class OnBoardFragment : Fragment() {
 
@@ -25,13 +25,28 @@ class OnBoardFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initialize()
-        visible()
         setupListener()
-        onClickToStart()
-        dotsindicator()
     }
 
-    private fun visible() = with(binding) {
+    private fun initialize() {
+        binding.viewPager2.adapter = OnBoardViewPagerAdapter(this@OnBoardFragment)
+        binding.dotsIndicator.attachTo(binding.viewPager2)
+    }
+
+    private fun setupListener() = with(binding) {
+        nextButton.setOnClickListener {
+            with(viewPager2) {
+                if (currentItem < 2) {
+                    setCurrentItem(viewPager2.currentItem + 1, true)
+                }
+            }
+        }
+        binding.start.setOnClickListener {
+            val preferenceHelper = PreferenceHelper()
+            preferenceHelper.unit(requireContext())
+            preferenceHelper.onSaveOnBoardState = false
+            findNavController().navigate(OnBoardFragmentDirections.actionOnBoardFragmentToNoteAppFragment())
+        }
         viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
                 when (position) {
@@ -51,30 +66,5 @@ class OnBoardFragment : Fragment() {
                 super.onPageSelected(position)
             }
         })
-
-    }
-
-    private fun onClickToStart() {
-        binding.start.setOnClickListener {
-            findNavController().navigate(OnBoardFragmentDirections.actionOnBoardFragmentToNoteAppFragment())
-        }
-    }
-
-    private fun initialize() {
-        binding.viewPager2.adapter = OnBoardViewPagerAdapter(this@OnBoardFragment)
-    }
-
-    private fun setupListener() = with(binding) {
-        nextButton.setOnClickListener {
-            with(viewPager2) {
-                if (currentItem < 2) {
-                    setCurrentItem(viewPager2.currentItem + 1, true)
-                }
-            }
-        }
-    }
-
-    private fun dotsindicator() {
-        binding.dotsIndicator.attachTo(binding.viewPager2)
     }
 }
