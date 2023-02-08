6443d20412dee6aa25e947f5b891bfa7ee6b6e23
ABOBA23462 <116728011+ABOBA23462@users.noreply.github.com>
2023-02-08 10:05:53 +0600

    Update OnBoardViewPagerAdapter.kt

--- app/src/main/java/com/example/kotlin2lvl_2less/ui/adapter/OnBoardViewPagerAdapter.kt
+++ app/src/main/java/com/example/kotlin2lvl_2less/ui/adapter/OnBoardViewPagerAdapter.kt
@@ -6,13 +6,12 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.kotlin2lvl_2less.ui.fragments.onboard.OnBoardPagingFragment
 
 class OnBoardViewPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
-    override fun getItemCount(): Int = 3
 
+    override fun getItemCount(): Int = 3
 
     override fun createFragment(position: Int): Fragment = OnBoardPagingFragment().apply {
         arguments = Bundle().apply {
             putInt(OnBoardPagingFragment.ARG_ONBOARD_PAGE_POSITION, position)
         }
-
     }
 }
